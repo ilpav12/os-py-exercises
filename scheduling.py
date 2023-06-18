d4608015c0444c97bd1cf1cb9a8e2613a8990828
Luca Pavan <ilpav@libero.it>
2023-06-18 20:01:54 +0200

    Added all scheduling algorithms and added more exercises

--- scheduling.py
+++ scheduling.py
@@ -1,3 +1,4 @@
+import copy
 import pandas as pd
 import matplotlib.pyplot as plt
 from abc import ABC, abstractmethod
@@ -5,7 +6,7 @@ from enum import Enum
 from matplotlib.colors import ListedColormap
 
 
-class States(Enum):
+class State(Enum):
     NONE = 0
     RUNNING = 1
     READY = 2
@@ -13,13 +14,22 @@ class States(Enum):
     CS = 4
 
 
+def get_state_names():
+    return [state.name for state in State]
+
+def get_state_values():
+    return [state.value for state in State]
+
+
 class Job:
-    def __init__(self, name: str, duration: int, starting_time: int, finishing_time: int = None,
+    def __init__(self, name: str, duration: int, starting_time: int, priority: int = 1,
+                 finishing_time: int = None,
                  response_time: int = None, completion_time: int = None, waiting_time: int = 0):
         self.name = name
         self.duration = duration
         self.remaining_time = duration
         self.starting_time = starting_time
+        self.priority = priority
         self.finishing_time = finishing_time
         self.response_time = response_time
         self.completion_time = completion_time
@@ -36,6 +46,9 @@ class Job:
     def get_states_values(self):
         return [state.value for state in self.states]
 
+    def get_num_state(self, state: State):
+        return self.states.count(state)
+
 
 class Jobs:
     def __init__(self, jobs: list[Job]):
@@ -47,12 +60,6 @@ class Jobs:
     def __getitem__(self, index):
         return self.jobs[index]
 
-    def append(self, job: Job):
-        self.jobs.append(job)
-
-    def remove(self, job: Job):
-        self.jobs.remove(job)
-
     def __len__(self):
         return len(self.jobs)
 
@@ -61,6 +68,18 @@ class Jobs:
 
     def __repr__(self):
         return str(self.to_array())
+
+    def append(self, job: Job):
+        self.jobs.append(job)
+
+    def remove(self, job: Job):
+        self.jobs.remove(job)
+
+    def copy(self):
+        return Jobs([job for job in self.jobs])
+
+    def deepcopy(self):
+        return Jobs([copy.deepcopy(job) for job in self.jobs])
 
     def to_array(self):
         return [job.to_array() for job in self.jobs]
@@ -72,6 +91,19 @@ class Jobs:
 
     def sort_by_name(self, reverse: bool = False):
         return Jobs(sorted(self.jobs, key=lambda x: x.name, reverse=reverse))
+
+    def avg_turnaround_time(self):
+        return sum([job.completion_time for job in self.jobs]) / len(self.jobs)
+
+    def avg_waiting_time(self):
+        return sum([job.waiting_time for job in self.jobs]) / len(self.jobs)
+
+    def avg_response_time(self):
+        return sum([job.response_time for job in self.jobs]) / len(self.jobs)
+
+    def throughput(self):
+        last_job = max(self.jobs, key=lambda x: x.finishing_time)
+        return len(self.jobs) / last_job.finishing_time
 
 
 class SchedulingAlgorithm(ABC):
@@ -95,29 +127,35 @@ class FCFS(SchedulingAlgorithm):
         return f'FCFS with dispatch latency {self.dispatch_latency}ms'
 
     def process_jobs(self, jobs: Jobs):
+        jobs_to_process = jobs.deepcopy()
         current_time = 0
         all_finished = False
         selected_job = None
         job_to_remove = None
         context_switch = self.dispatch_latency
         processed_jobs = Jobs([])
+        ready_queue = Jobs([job for job in jobs_to_process if job.starting_time == current_time])
         while not all_finished:
-            if len(jobs) == 0:
+            if len(jobs_to_process) == 0:
                 all_finished = True
                 continue
 
             # Select the job with the maximum waiting time if no job is selected
             if selected_job is None:
-                selected_job = max(jobs, key=lambda x: x.waiting_time)
+                selected_job = max(ready_queue, key=lambda x: x.waiting_time)
+                selected_job.waiting_time = 0
                 context_switch = self.dispatch_latency
 
-            for job in jobs:
+            for job in jobs_to_process:
                 if job.starting_time > current_time:  # Job has not arrived yet
-                    job.states.append(States.NONE)
+                    job.states.append(State.NONE)
                     continue
 
+                if job not in ready_queue:
+                    ready_queue.append(job)
+
                 if job != selected_job:
-                    job.states.append(States.READY)
+                    job.states.append(State.READY)
                     job.waiting_time += 1
                     continue
 
@@ -125,12 +163,12 @@ class FCFS(SchedulingAlgorithm):
                     job.response_time = current_time + self.dispatch_latency - job.starting_time
 
                 if context_switch > 0:
-                    job.states.append(States.CS)
+                    job.states.append(State.CS)
                     context_switch -= 1
                     continue
 
                 job.remaining_time -= 1
-                job.states.append(States.RUNNING)
+                job.states.append(State.RUNNING)
 
                 if job.remaining_time == 0:
                     job.finishing_time = current_time + self.dispatch_latency
@@ -142,7 +180,7 @@ class FCFS(SchedulingAlgorithm):
                     selected_job = None
 
             if job_to_remove is not None:
-                jobs.remove(job_to_remove)
+                jobs_to_process.remove(job_to_remove)
                 job_to_remove = None
 
             current_time += 1
@@ -158,6 +196,7 @@ class RR(SchedulingAlgorithm):
         return f'RR with dispatch latency {self.dispatch_latency}ms and quantum {self.quantum}ms'
 
     def process_jobs(self, jobs: Jobs):
+        jobs_to_process = jobs.deepcopy()
         current_time = 0
         all_finished = False
         change_selected_job = True
@@ -167,25 +206,32 @@ class RR(SchedulingAlgorithm):
         processed_jobs = Jobs([])
         quantum = self.quantum
         while not all_finished:
-            if len(jobs) == 0:
+            if len(jobs_to_process) == 0:
                 all_finished = True
                 continue
 
+            ready_queue = Jobs([job for job in jobs_to_process if job.starting_time <= current_time])
+
             # Select the job with the maximum waiting time if no job is selected
             if change_selected_job:
-                new_selected_job = max(jobs, key=lambda x: x.waiting_time)
+                # filter most_waiting_jobs by taking only the ones with the highest priority
+                highest_priority = min(ready_queue, key=lambda x: x.priority).priority
+                highest_priority_jobs = Jobs([job for job in ready_queue if job.priority == highest_priority])
+                most_waiting_jobs = Jobs([job for job in highest_priority_jobs if job.waiting_time == max(highest_priority_jobs, key=lambda x: x.waiting_time).waiting_time])
+                new_selected_job = min(most_waiting_jobs, key=lambda x: x.get_num_state(State.RUNNING))
                 if selected_job != new_selected_job:
                     context_switch = self.dispatch_latency
                 selected_job = new_selected_job
+                selected_job.waiting_time = 0
                 change_selected_job = False
 
-            for job in jobs:
+            for job in jobs_to_process:
                 if job.starting_time > current_time:  # Job has not arrived yet
-                    job.states.append(States.NONE)
+                    job.states.append(State.NONE)
                     continue
 
                 if job != selected_job:
-                    job.states.append(States.READY)
+                    job.states.append(State.READY)
                     job.waiting_time += 1
                     continue
 
@@ -193,13 +239,13 @@ class RR(SchedulingAlgorithm):
                     job.response_time = current_time + self.dispatch_latency - job.starting_time
 
                 if context_switch > 0:
-                    job.states.append(States.CS)
+                    job.states.append(State.CS)
                     context_switch -= 1
                     continue
 
                 quantum -= 1
                 job.remaining_time -= 1
-                job.states.append(States.RUNNING)
+                job.states.append(State.RUNNING)
 
                 if job.remaining_time == 0:
                     job.finishing_time = current_time + self.dispatch_latency
@@ -216,15 +262,210 @@ class RR(SchedulingAlgorithm):
                     change_selected_job = True
 
             if job_to_remove is not None:
-                jobs.remove(job_to_remove)
+                jobs_to_process.remove(job_to_remove)
                 job_to_remove = None
 
             current_time += 1
 
         return processed_jobs.sort_by_name()
 
+class SJF(SchedulingAlgorithm):
+    def __init__(self, dispatch_latency: int = 0):
+        super().__init__(dispatch_latency)
+
+    def __str__(self):
+        return f'SJF with dispatch latency {self.dispatch_latency}ms'
+
+    def process_jobs(self, jobs: Jobs):
+        jobs_to_process = jobs.deepcopy()
+        current_time = 0
+        all_finished = False
+        selected_job = None
+        job_to_remove = None
+        context_switch = self.dispatch_latency
+        processed_jobs = Jobs([])
+        while not all_finished:
+            if len(jobs_to_process) == 0:
+                all_finished = True
+                continue
+
+            # Select the job with the minimum remaining time if no job is selected
+            if selected_job is None:
+                jobs_to_select = [job for job in jobs_to_process if job.starting_time <= current_time]
+                selected_job = min(jobs_to_select, key=lambda x: x.duration)
+                context_switch = self.dispatch_latency
+
+            for job in jobs_to_process:
+                if job.starting_time > current_time:  # Job has not arrived yet
+                    job.states.append(State.NONE)
+                    continue
+
+                if job != selected_job:
+                    job.states.append(State.READY)
+                    job.waiting_time += 1
+                    continue
+
+                if job.response_time is None:
+                    job.response_time = current_time + self.dispatch_latency - job.starting_time
+
+                if context_switch > 0:
+                    job.states.append(State.CS)
+                    context_switch -= 1
+                    continue
+
+                job.remaining_time -= 1
+                job.states.append(State.RUNNING)
+
+                if job.remaining_time == 0:
+                    job.finishing_time = current_time + self.dispatch_latency
+                    if self.dispatch_latency == 0: job.finishing_time += 1
+                    job.completion_time = job.finishing_time - job.starting_time
+                    job.waiting_time = job.completion_time - job.duration
+                    processed_jobs.append(job)
+                    job_to_remove = job
+                    selected_job = None
+
+            if job_to_remove is not None:
+                jobs_to_process.remove(job_to_remove)
+                job_to_remove = None
+
+            current_time += 1
+
+        return processed_jobs.sort_by_name()
+
+class SRTF(SchedulingAlgorithm):
+    def __init__(self, dispatch_latency: int = 0):
+        super().__init__(dispatch_latency)
+
+    def __str__(self):
+        return f'SRTF with dispatch latency {self.dispatch_latency}ms'
+
+    def process_jobs(self, jobs: Jobs):
+        jobs_to_process = jobs.deepcopy()
+        current_time = 0
+        all_finished = False
+        selected_job = None
+        job_to_remove = None
+        context_switch = self.dispatch_latency
+        processed_jobs = Jobs([])
+        while not all_finished:
+            if len(jobs_to_process) == 0:
+                all_finished = True
+                continue
+
+            # Select the job with the minimum remaining time if no job is selected
+            jobs_to_select = [job for job in jobs_to_process if job.starting_time <= current_time]
+            new_selected_job = min(jobs_to_select, key=lambda x: x.remaining_time)
+            if selected_job != new_selected_job:
+                context_switch = self.dispatch_latency
+            selected_job = min(jobs_to_select, key=lambda x: x.remaining_time)
+
+            for job in jobs_to_process:
+                if job.starting_time > current_time:  # Job has not arrived yet
+                    job.states.append(State.NONE)
+                    continue
+
+                if job != selected_job:
+                    job.states.append(State.READY)
+                    job.waiting_time += 1
+                    continue
+
+                if job.response_time is None:
+                    job.response_time = current_time + self.dispatch_latency - job.starting_time
+
+                if context_switch > 0:
+                    job.states.append(State.CS)
+                    context_switch -= 1
+                    continue
+
+                job.remaining_time -= 1
+                job.states.append(State.RUNNING)
+
+                if job.remaining_time == 0:
+                    job.finishing_time = current_time + self.dispatch_latency
+                    if self.dispatch_latency == 0: job.finishing_time += 1
+                    job.completion_time = job.finishing_time - job.starting_time
+                    job.waiting_time = job.completion_time - job.duration
+                    processed_jobs.append(job)
+                    job_to_remove = job
+
+            if job_to_remove is not None:
+                jobs_to_process.remove(job_to_remove)
+                job_to_remove = None
+
+            current_time += 1
+
+        return processed_jobs.sort_by_name()
+
+class HRRF(SchedulingAlgorithm):
+    def __init__(self, dispatch_latency: int = 0):
+        super().__init__(dispatch_latency)
+
+    def __str__(self):
+        return f'HRRF with dispatch latency {self.dispatch_latency}ms'
+
+    def process_jobs(self, jobs: Jobs):
+        jobs_to_process = jobs.deepcopy()
+        current_time = 0
+        all_finished = False
+        selected_job = None
+        job_to_remove = None
+        context_switch = self.dispatch_latency
+        processed_jobs = Jobs([])
+        while not all_finished:
+            if len(jobs_to_process) == 0:
+                all_finished = True
+                continue
+
+            # Select the job with the minimum remaining time if no job is selected
+            if selected_job is None:
+                jobs_to_select = [job for job in jobs_to_process if job.starting_time <= current_time]
+                new_selected_job = max(jobs_to_select, key=lambda x: (x.duration + x.waiting_time) / x.duration)
+                if selected_job != new_selected_job:
+                    context_switch = self.dispatch_latency
+                selected_job = new_selected_job
+
+            for job in jobs_to_process:
+                if job.starting_time > current_time:  # Job has not arrived yet
+                    job.states.append(State.NONE)
+                    continue
+
+                if job != selected_job:
+                    job.states.append(State.READY)
+                    job.waiting_time += 1
+                    continue
+
+                if job.response_time is None:
+                    job.response_time = current_time + self.dispatch_latency - job.starting_time
+
+                if context_switch > 0:
+                    job.states.append(State.CS)
+                    context_switch -= 1
+                    continue
+
+                job.remaining_time -= 1
+                job.states.append(State.RUNNING)
+
+                if job.remaining_time == 0:
+                    job.finishing_time = current_time + self.dispatch_latency
+                    if self.dispatch_latency == 0: job.finishing_time += 1
+                    job.completion_time = job.finishing_time - job.starting_time
+                    job.waiting_time = job.completion_time - job.duration
+                    processed_jobs.append(job)
+                    job_to_remove = job
+                    selected_job = None
+
+            if job_to_remove is not None:
+                jobs_to_process.remove(job_to_remove)
+                job_to_remove = None
+
+            current_time += 1
+
+        return processed_jobs.sort_by_name()
+
+
 def plot_gantt_chart(jobs: Jobs):
-    jobs_states = [state.name for state in States]
+    job_state_names = get_state_names()
     # create custom colormap with custom colors for each state using a dictionary
     cmap = ListedColormap(['white', 'red', 'blue', 'yellow', 'gray'])
 
@@ -248,12 +489,12 @@ def plot_gantt_chart(jobs: Jobs):
         marker='_',
         lw=20,
         cmap=cmap,
-        vmin=0, vmax=len(jobs_states) - 1,
+        vmin=0, vmax=len(job_state_names) - 1,
     )
     # make color legend for each state
-    handles = [plt.plot([], [], c=cmap(i), lw=10, marker="_")[0] for i in range(len(jobs_states))]
-    labels = jobs_states
+    handles = [plt.plot([], [], c=cmap(i), lw=10, marker="_")[0] for i in range(len(job_state_names))]
+    labels = job_state_names
     # put legend outside the plot on the bottom
     plt.legend(handles, labels, loc=(0.2, -0.3), numpoints=1, fontsize=12,
-               ncol=len(jobs_states))
+               ncol=len(job_state_names))
     plt.show()
